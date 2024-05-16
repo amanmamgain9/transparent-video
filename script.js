9e98cc1162d03a37d47337c2656e338729280901
amanmamgain9 <amanmamgain9@gmail.com>
2024-05-16 20:47:30 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js - Additional JavaScript functionality can be added here if needed
+document.addEventListener('DOMContentLoaded', () => {
+    const video = document.getElementById('transparentVideo');
+    video.play();
+});
